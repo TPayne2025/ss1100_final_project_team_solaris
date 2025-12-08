5a124dedc34f06653681ae8c10c98a0e2b5b4a04
gabbyleonard <gabriella.leonard@nps.edu>
2025-12-07 17:52:20 -0800

    Update rcs_script.py _ 07DEC

--- RCS/rcs_script.py
+++ RCS/rcs_script.py
@@ -1,17 +1,35 @@
+max_thrust= 100
+max_flow_rate =0.05
+max_velocity= 2000
+thruster_names = {0:"Thruster 1", 1: "Thruster 2", 2: "Thruster 3"}
+
 def malfunction_detection(thrust_values):
     """
     Checks input thrust values and angles for each thruster against set limits.
     If a thruster is outside of these limits, it prints a message.
     """
-    # YOUR CODE HERE
+    #
+    for i, (mass_flow_rate, exhaust_velocity, time_elapsed) in enumerate(thrust_values):
+        thruster_name= thruster_names[i]
+        thrust=mass_flow_rate*exhaust_velocity
+        if thrust> max_thrust:
+          thrust_diff= thrust-max_thrust
+          print(f"{thruster_name}: Thrust exceeds maximum by {thrust_diff:.2f} N")
+        if mass_flow_rate>max_flow_rate:
+            print(f"{thruster_name}: Flow rate exceeds maximum by {mass_flow_rate-max_flow_rate:.2f} kg/s")
+        if exhaust_velocity>max_velocity:
+            print(f"{thruster_name}: Velocity exceeds maximum by {exhaust_velocity-max_velocity:.2f} m/s")
+        
     pass
 
 def velocity_change_calculation(mass_flow_rate, exhaust_velocity, time_elapsed):
     """
     Performs calculations to determine the change in velocity resulting from a maneuver event.
     """
-    # YOUR CODE HERE
-    return 0
+    thrust = mass_flow_rate * exhaust_velocity
+    delta_v = (thrust * time_elapsed)/500
+
+    return delta_v
 
 def main():
     """
@@ -29,6 +47,9 @@ def main():
         delta_v = velocity_change_calculation(m_dot, v_e, t)
         print(f"Calculated delta_v: {delta_v:.2f} m/s")
         print("-" * 20)
+
+    print("\n--- Malfunction Detection ---")
+    malfunction_detection(test_cases)
 
 if __name__ == "__main__":
     main()
